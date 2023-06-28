260589c25a03526f1593d8f96499a9bcf9ca49d5
Rie Kaneko <bogorie@gmail.com>
2023-06-27 23:31:25 -0500

    Created main structure

---
+++ internal/service/const.go
@@ -0,0 +1,6 @@
+package service
+
+const (
+	layout       = "1/2/2006"
+	templatePath = "template/email.html"
+)
--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"os"
+	"rie-kaneko/credit-cards-summary/config"
+	"rie-kaneko/credit-cards-summary/internal/service"
+)
+
+var s *service.Service
+
+func init() {
+	s = &service.Service{
+		Config: &config.Config,
+		Log:    config.InitLogrus(config.Config.Environment.LogLevel),
+	}
+}
 
 func main() {
-	fmt.Println("Hello, World")
+	if err := s.Run(); err != nil {
+		os.Exit(0)
+	}
 }
